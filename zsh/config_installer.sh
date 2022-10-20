59e6fc836ce02f1d514f7847cf7b11223c8b249a
TE1ch0st <vitaliktimtsurask@gmail.com>
2022-10-20 21:23:48 +0300

    Fix zsh

--- zsh/config_installer.sh
+++ zsh/config_installer.sh
@@ -9,8 +9,11 @@
 #	   $$ |   $$$$$$$$\ $$$$$$\ \$$$$$$$\ $$ |  $$ |\$$$$$$  /$$$$$$$  |  \$$$$  |
 #	   \__|   \________|\______| \_______|\__|  \__| \______/ \_______/    \____/  
 
+# zsh install
+sudo apt install zsh
+
 # Oh my zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)
 echo 'oh-my-zsh is installed!'
 
 # Plugins
@@ -21,8 +24,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 mkdir -p ~/.te1ch0st
 cp -f te1ch0st.py ~/.te1ch0st/te1ch0st.py
 cp -f zshrc ~/.zshrc
-apt install python3
-apt install python3-pip
-pip3 install colorama
+sudo apt install python3
+sudo apt install python3-pip
+sudo pip3 install colorama
 
 echo "ZSH config is installed!"
