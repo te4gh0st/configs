cb831440a28e7e6f8abd0c094e3d59df8ea31e89
TE1ch0st <vitaliktimtsurask@gmail.com>
2022-10-31 22:02:32 +0300

    Fix errors
    1.ZSH fix plugins
    2.Nvim fix logo commit error

--- nvim/config_installer.sh
+++ nvim/config_installer.sh
@@ -16,3 +16,4 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 echo "Nvim config is installed!"
+nvim
